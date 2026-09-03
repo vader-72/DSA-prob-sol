8e8ee1343f5fe9706862ec78fbe0b075699d1e59
Sadiq <sadiqmohammad414@gmail.com>
2026-09-03 22:49:59 +0530

    Time: 31 ms (58.73%), Space: 67.2 MB (68.69%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+    
+ */
+var groupAnagrams = function(strs) {
+    
+    let map = new Map();
+
+    for (let str of strs) {
+        let sorted = str.split("").sort().join("");
+
+        if (!map.has(sorted)) {
+            map.set(sorted, []);
+        }
+
+        map.get(sorted).push(str);
+    }
+
+    return Array.from(map.values());
+
+    
+    
+    
+ 
+};
