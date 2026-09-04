9be804cef17059a77b93b8b5406298b3165172c1
Sadiq <sadiqmohammad414@gmail.com>
2026-09-04 11:17:48 +0530

    Time: 0 ms (100%), Space: 54.5 MB (31.39%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.js
+++ 0771-jewels-and-stones/0771-jewels-and-stones.js
@@ -4,15 +4,12 @@
  * @return {number}
  */
 var numJewelsInStones = function(jewels, stones) {
-    let count = 0
-    for(let jls of jewels){
-        for(let st of stones){
-        if(st === jls){
-            count++
-        }
+   let jls = new Set(jewels)
+   let count = 0
+   for(let st of stones){
+    if(jls.has(st))
+     count++
+   }
 
-       }
-    
-    }
-    return count
+   return count
 };
