265161638d831b8ba442d636f556fb3e292a89a3
Sadiq <sadiqmohammad414@gmail.com>
2026-09-05 11:29:25 +0530

    Time: 0 ms (100%), Space: 55 MB (31.6%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+   let prefix=''
+   for(let i=0 ; i<strs[0].length ; i++){
+   
+    for(let j = 1 ; j<strs.length ; j++){
+        if(strs[0][i] !== strs[j][i]){
+            return prefix;
+        }
+    }
+    prefix+=strs[0][i]
+   
+    
+   }
+   return prefix
+
+    
+};
