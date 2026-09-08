bdea127ca0493af3667e7e625a9f19f29fc1b451
Sadiq <sadiqmohammad414@gmail.com>
2026-09-08 10:08:48 +0530

    Time: 0 ms (100%), Space: 55.8 MB (5.34%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+  let pas = []
+  for(let i = 0 ; i<=rowIndex ; i++){
+    let row = [1]
+    for(let j=1 ; j<i ; j++){
+        row.push(
+          pas[i - 1][j - 1] + pas[i-1][j]
+        )
+    }
+      if(i>0){
+            row.push(1)
+        }
+        pas.push(row)
+  }
+  return pas[rowIndex]
+};
