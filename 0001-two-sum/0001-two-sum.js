bc7b271dd4a6feac1aaebc6a051d10ca8ac7cd36
Sadiq <sadiqmohammad414@gmail.com>
2026-09-10 15:09:58 +0530

    Time: 32 ms (36.44%), Space: 54.3 MB (77.8%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,13 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-   for(let i = 0 ; i<nums.length ; i++){
-    for(let j=i+1 ; j<nums.length; j++){
-        if(nums[i] + nums[j] === target){
-            return [i , j]
+  for(let i =0 ; i<nums.length ; i++){
+    for(let j =i +1 ; j<nums.length ; j++){
+        if(nums[i] === target - nums[j]){
+            return nums = [ i, j]
         }
     }
-   }
+  }
+
+   
+   
+ 
  
     
    
