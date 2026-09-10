0256af9fd91429022cb7bed36357bfb76d9cbad2
Sadiq <sadiqmohammad414@gmail.com>
2026-09-10 22:26:46 +0530

    Time: 22 ms (19.54%), Space: 65 MB (5.83%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+   let arr = new Map()
+   for(let num of nums){
+     arr.set(num , (arr.get(num) || 0)+ 1)
+   } 
+   const bucket = Array.from(
+    {length:nums.length +1 },
+    () => []
+   )
+   for(let [num,freq] of arr){
+     bucket[freq].push(num)
+   }
+   
+   const result = []
+
+   for(let i =bucket.length -1  ; i>=0 ; i--){
+     for(let num of bucket[i])
+     result.push(num);
+     if(result.length ===k){
+        return result
+     }
+   } 
+};
