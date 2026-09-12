53e1f299d7829bb10dcadf496e36448dd63a5635
Sadiq <sadiqmohammad414@gmail.com>
2026-09-12 23:30:11 +0530

    Time: 26 ms (10.74%), Space: 62.3 MB (7.46%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    let map = new Map()
+
+    for(let char of magazine){
+        if(map.has(char)){
+            map.set(char, map.get(char) +1)
+        }else{
+            map.set(char, 1)
+        }
+    }
+
+    for(let char of ransomNote){
+        if(!map.has(char) || map.get(char) === 0){
+            return false
+        }
+        map.set(char,map.get(char) - 1 )
+    }
+    return true
+};
