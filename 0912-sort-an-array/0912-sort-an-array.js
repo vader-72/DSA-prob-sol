8e77c4e93513cc176c51896b70b5a2d786a2864f
Sadiq <sadiqmohammad414@gmail.com>
2026-09-16 23:39:07 +0530

    Time: 105 ms (45.94%), Space: 79.6 MB (60.58%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+  function merge(left ,right){
+    let result = []
+    let i =0 ;
+    let j = 0;
+    while(i<left.length && j<right.length){
+        if(left[i] < right[j]){
+            result.push(left[i])
+            i++
+        }else{
+            result.push(right[j])
+            j++
+        }
+    }
+
+    while(i<left.length ){
+        result.push(left[i])
+        i++
+    }
+    
+    while(j<right.length){
+        result.push(right[j])
+        j++
+    }
+    return result 
+  }
+  function mergeSort(arr){
+        if(arr.length <=1){
+            return arr
+        }
+
+        let mid = Math.floor(arr.length/2)
+        
+        let left = mergeSort(arr.slice(0 , mid))
+        let right = mergeSort(arr.slice(mid))
+
+        return merge(left ,right)
+    }
+
+    return mergeSort(nums)
+  
+};
