9b31df664b216ed0bd01b7fd8e507f2453cbeb46
Sadiq <sadiqmohammad414@gmail.com>
2026-09-16 23:51:15 +0530

    Time: 90 ms (61.2%), Space: 79.1 MB (65.08%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -4,10 +4,10 @@
  */
 var sortArray = function(nums) {
   function merge(left ,right){
-    let result = []
-    let i =0 ;
-    let j = 0;
-    while(i<left.length && j<right.length){
+    let result = [];
+    let i = 0 ;
+    let j = 0 ;
+    while(i<left.length && j < right.length){
         if(left[i] < right[j]){
             result.push(left[i])
             i++
@@ -17,30 +17,32 @@ var sortArray = function(nums) {
         }
     }
 
-    while(i<left.length ){
+    while(i< left.length ){
         result.push(left[i])
         i++
     }
-    
-    while(j<right.length){
+
+    while(j< right.length){
         result.push(right[j])
         j++
     }
-    return result 
+
+    return result
   }
+
+
   function mergeSort(arr){
-        if(arr.length <=1){
-            return arr
-        }
 
-        let mid = Math.floor(arr.length/2)
-        
-        let left = mergeSort(arr.slice(0 , mid))
-        let right = mergeSort(arr.slice(mid))
-
-        return merge(left ,right)
+    if(arr.length <=1){
+        return arr
     }
 
-    return mergeSort(nums)
-  
+    let mid = Math.floor(arr.length/2)
+    let left = mergeSort(arr.slice(0 , mid))
+    let right = mergeSort(arr.slice(mid))
+
+    return merge(left,right)
+  }
+
+  return mergeSort(nums)
 };
